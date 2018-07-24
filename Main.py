e0d2cd80995323ffd4d78daf46bdfee05b78a09b
vimanyu <vimanyukachroo@yahoo.com>
2018-07-24 16:31:57 +0100

    Add files via upload
    code update.
    input from the user to fetch repository information and user profile.

--- Main.py
+++ Main.py
@@ -15,9 +15,23 @@ api = Api(app)
 
 class main():
     def __init__(self):
-        self.number_of_users = input ('Enter the number username/repos you want to enter: ')
+        self.number_of_users = int(input ('Enter the number username/repos you want to enter: '))
+        self.git_username = []
+        self.git_repo = []
         
+        for n in range(0,self.number_of_users):
+            self.user_name = input('Enter the user name: ')
+            self.repo_name = input('Enter the repository name: ')
+            self.git_username.append(self.user_name)
+            self.git_repo.append(self.repo_name)
         
+#        print(self.git_username)
+#        print(self.git_repo)
+ 
+        user = requests.get('https://api.github.com/repos/'+ self.git_username[0] +'/' + self.git_repo[0] + '/commits')
+        git_data = json.loads(user.text)
+        print(git_data)
+    
 if __name__ == "__main__":
     Git = main()
     app.run(port = 8080)
