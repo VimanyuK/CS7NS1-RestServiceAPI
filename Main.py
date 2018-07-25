95f69d37911439a1e374d93c2e6465f72e37e817
vimanyu <vimanyukachroo@yahoo.com>
2018-07-25 12:53:20 +0100

    Add files via upload
    update to fetch n number of repos and store them in a dictionary for further use.

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ Created on Mon Jul 23 14:17:14 2018
 
 from flask import Flask
 from flask_restful import Resource,Api, reqparse
-import os,sys,requests,time, json
+import requests,json
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,21 +19,29 @@ class main():
         self.git_username = []
         self.git_repo = []
         
-        for n in range(0,self.number_of_users):
+        while(self.number_of_users != 0):
             self.user_name = input('Enter the user name: ')
-            self.repo_name = input('Enter the repository name: ')
             self.git_username.append(self.user_name)
+            self.repo_name = input('Enter the repository name: ')
             self.git_repo.append(self.repo_name)
+            self.number_of_users = self.number_of_users - 1
         
-#        print(self.git_username)
-#        print(self.git_repo)
- 
-        user = requests.get('https://api.github.com/repos/'+ self.git_username[0] +'/' + self.git_repo[0] + '/commits')
-        git_data = json.loads(user.text)
-        print(git_data)
+        self.git_data = {}
+        for n in range(0,len(self.git_username)):
+            user = requests.get('https://api.github.com/repos/'+ self.git_username[n] +'/' + self.git_repo[n] + '/commits')
+            self.git_data['{0}'.format(self.git_username[n])] = json.loads(user.text)
+#            git_data = json.loads(user.text)
+            print(self.git_data)
     
 if __name__ == "__main__":
     Git = main()
     app.run(port = 8080)
         
         
+    
+    
+    
+
+            
+            
+            
