2eabc32251f4f2e65ddc3dff7cbdb52763e062e1
vimanyu <vimanyukachroo@yahoo.com>
2018-07-26 16:25:04 +0100

    Add files via upload
    code to get total commits in the year 2018 for all the repositories for the given user names.

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ Created on Mon Jul 23 14:17:14 2018
 from flask import Flask
 from flask_restful import Resource,Api, reqparse
 import requests,json, subprocess, datetime
+from nested_lookup import nested_lookup
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,25 +31,28 @@ class main():
         for n in range(0,len(self.git_username)):
             user = requests.get('https://api.github.com/repos/'+ self.git_username[n] +'/' + self.git_repo[n] + '/commits')
             self.git_data['{0}'.format(self.git_username[n])] = json.loads(user.text)
-#            git_data = json.loads(user.text)
-            print(self.git_data)
+#            print(self.git_data)
     
+        self.git_commit_count = {}
+        for i in range(0,len(self.git_username)):
+            git_user_data = {}
+            temp = subprocess.check_output(["curl", "-H", "Accept: application/vnd.github.cloak-preview", "https://api.github.com/search/commits?q=author:"+self.git_username[i]+"&per_page=1000"], shell=False).decode('utf-8')
+            git_user_data['{0}'.format(self.git_username[i])] = json.loads(temp)
+            results = nested_lookup(key = 'date', document = git_user_data, wild = True, with_keys = False)
+            results = list(set(results))
+            results = [x for x in results if ("2018" in x)]
+            self.git_commit_count['{0}'.format(self.git_username[i])] = len(results)
+        print(self.git_commit_count)
+
+
 if __name__ == "__main__":
     Git = main()
     app.run(port = 8080)
 
 
-###### for total commits and converting unix date time
-op = subprocess.check_output([
-    "curl", "-H", "Accept: application/vnd.github.cloak-preview", "https://api.github.com/search/commits?q=author:vimanyuK"], shell=False)
-op = op.decode('utf-8')
-info = json.loads(op)
-    
+###### for converting unix timestamp
 print(
     datetime.datetime.fromtimestamp(
         int("1531612800")
     ).strftime('%Y-%m-%d %H:%M:%S')
 )
-    
-                
-            
